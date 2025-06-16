5cba944e20c323284d19c71d1ae4c8ea2e77e88f
Golovanov, Konstantin <kgolovanov@itkey.com>
2025-06-16 22:18:06 +0200

    Refactor polynomial logic and enhance tests:
    - Introduced abstract `Polynomial` class for generalized polynomial representation.
    - Added `PolynomialTerm` class with improved validation and string handling.
    - Implemented `PolynomialFactory` for creating polynomials based on degree.
    - Refactored `PolynomParser` for better string normalization and parsing logic.
    - Expanded test cases to include positive and negative scenarios for `PolynomialTerm` and `PolynomParser`.

--- polynominal.py
+++ polynominal.py
@@ -1,99 +1,222 @@
 import re
-
-class PolynomSecondDegree:
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
-        self._discriminant = None
-
-    @property
-    def discriminant(self):
-        if not self._discriminant:
-            self._discriminant = self.b ** 2 - 4 * self.a * self.c
+from abc import ABC, abstractmethod
+from typing import List, Tuple
 
 
-    def sulutions_count(self):
-        if self.discriminant > 0:
-            return 2
-        elif self.discriminant == 0:
-            return 1
-        else:
-            return 0
-
-
-class PolynomTerm:
+class PolynomialTerm:
     def __init__(self, coefficient, degree):
         self.coefficient = coefficient
         self.degree = degree
 
     @classmethod
-    def from_string(cls, term_str):
+    def from_string(cls, term_str: str):
         """Create a PolynomTerm from a string representation."""
-        pattern = r"^(?P<sign>[\-+])?(?P<coefficient>\d*)?(?P<x_part>X(\^(?P<degree>\d+))?)?$"
-        match = re.match(pattern, term_str)
-        if not match:
-            raise ValueError(f"Invalid term string: {term_str}")
-        sign = match.group("sign")
-        coefficient = match.group("coefficient")
-        x_part = match.group("x_part")
-        degree = match.group("degree")
-        if sign == '-':
-            if coefficient is None or coefficient == '':
-                coefficient = -1
-            else:
-                coefficient = -int(coefficient)
-        else:
-            if coefficient is None or coefficient == '':
-                coefficient = 1
-            else:
-                coefficient = int(coefficient)
-        if x_part is None:
-            degree = 0
-        elif degree is None:
-            degree = 1
-        else:
-            degree = int(degree)
+        original_str = term_str
+        if not isinstance(term_str, str):
+            raise ValueError("Invalid term string: not a string.")
+        if "X" not in term_str:
+            term_str += "*X^0"
+        if term_str.endswith("X"):
+            term_str += "^1"
+        if term_str.startswith("X") or term_str.startswith("+X"):
+            term_str = "1*" + term_str
+        elif term_str.startswith("-X"):
+            term_str = "-1*" + term_str[1:]
+
+        if term_str.count("*") != 1 or term_str.count(
+                "^") != 1 or term_str.count("X") != 1:
+            raise ValueError(f"Invalid term - \"{original_str}\": invalid format.")
+        coefficient, degree = term_str.split("*")
+        coefficient = int(coefficient)
+        degree = int(degree.replace("X^", ""))
         return cls(coefficient, degree)
 
-        
+    def __str__(self):
+        return f"{self.coefficient}*X^{self.degree}"
+
+    def __repr__(self):
+        return f"PolynomialTerm({self.coefficient}, {self.degree})"
+
+    def __eq__(self, other):
+        return self.degree == other.degree and self.coefficient == other.coefficient
+
+
+class Polynomial(ABC):
+    """
+    Representation of a general polynomial using terms with coefficients and degrees.
+    """
+
+    def __init__(self, terms: List[PolynomialTerm]):
+        """
+        Represents a polynomial equation using a list of polynomial terms.
+
+        This class initializes with a list of PolynomialTerm objects, which represent
+        the individual terms of a polynomial. It ensures that terms are sorted by their
+        degree and performs a reduction step to simplify the polynomial by combining
+        like terms if applicable.
+
+        :param terms: Dictionary of terms in the polynomial.
+        :type terms: List[PolynomialTerm]
+        """
+        self._degree = None
+        self._solutions_count = None
+        self._terms = None
+
+        # setting
+        self.terms = terms
+        self.degree  # noqa
+
+    @property
+    @abstractmethod
+    def solutions_count(self) -> int:
+        """Returns the number of solutions for the polynomial."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_reduced_form(self):
+        """Returns the reduced form of the polynomial."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_solutions(self) -> Tuple[float]:
+        """Returns the solutions for the polynomial."""
+        raise NotImplementedError
+
+    @property
+    def degree(self):
+        """Returns the degree of the polynomial."""
+        if not self._degree:
+            self._degree = self.find_max_degree(self.terms)
+        return self._degree
+
+    @property
+    def terms(self):
+        return self._terms
+
+    @terms.setter
+    def terms(self, value: List[PolynomialTerm]) -> None:
+        """
+        Sets the terms of the polynomial while reducing terms to the same degree by
+        combining their coefficients.
+
+        This setter method processes the list of polynomial terms and consolidates duplicate
+        terms by degree, ensuring that each degree has a single term with a summed coefficient.
+
+        :param value: A list of polynomial terms to be set in the polynomial. Each term
+            is an instance of the PolynomialTerm class and contains a degree and
+            coefficient.
+        :type value: List[PolynomialTerm]
+        :return: None
+        """
+        if self.terms:
+            raise ValueError("Terms cannot be set after initialization.")
+        if not isinstance(value, list):
+            raise ValueError(
+                "Value of terms must be a list of PolynomialTerm objects.")
+        reduced_terms = {}
+        for term in value:
+            if not isinstance(term, PolynomialTerm):
+                raise ValueError(
+                    "Value of terms must be a list of PolynomialTerm objects.")
+            if term.degree not in reduced_terms.keys():
+                reduced_terms[term.degree] = term
+            else:
+                reduced_terms[term.degree].coefficient += term.coefficient
+
+        self._terms = sorted(reduced_terms.values(), key=lambda x: x.degree)
+
+    @staticmethod
+    def find_max_degree(terms: List[PolynomialTerm]) -> int:
+        """Returns the highest degree among all polynomial terms.
+
+        Args:
+            terms: List of polynomial terms
+
+        Returns:
+            The maximum degree value found in terms
+        """
+        return max((term.degree for term in terms), default=0)
+
+
+class PolynomialFactory:
+    """Factory class for creating polynomials of different degrees."""
+
+    def __init__(self, parser):
+        self.parser = parser
+        self.polynomials = {
+            1: PolynomialFirstDegree,
+            2: PolynomialSecondDegree
+        }
+
+    def create(self, polynom_str):
+        """Create a polynomial object based on the polynomial string."""
+        polynom_terms = self.parser.parse(polynom_str)
+        degree = self.parser.degree
+        if degree not in self.polynomials.keys():
+            raise ValueError(f"Polynomial of degree {degree} is not supported.")
+        return self.polynomials[degree](polynom_terms)
+
+
+class PolynomialFirstDegree(Polynomial):
+    def __init__(self, terms):
+        super().__init__(terms)
+
+
+class PolynomialSecondDegree(Polynomial):
+    def __init__(self, terms):
+        super().__init__(terms)
+        self._discriminant = None
 
 
 class PolynomParser:
-    pattern = r"^[\dX^\-+*]+=[\dX^+\-*]+$" # Regex pattern to match a polynomial string
+    pattern = r"^[\dX\^\-\+\*=]+$"  # Regex pattern to match a polynomial string
 
-    def __init__(self, polynom_str):
-        """Initialize the parser with a polynomial string."""
-        normalized_str = self.normalize(polynom_str)
-        self.polynom_str = self.parse(normalized_str)
-
-    def normalize(self):
-        """Normalize the polynomial string by removing spaces and ensuring it matches the expected format."""
-        if not isinstance(self.polynom_str, str):
-            raise ValueError("Invalid polynomial string to parse: not a string.")
+    @classmethod
+    def normalize(cls, polynom_str: str):
+        """Normalize and validate a polynomial string."""
+        if not isinstance(polynom_str, str):
+            raise ValueError(
+                "Invalid polynomial string to parse: not a string.")
+        if polynom_str.count("=") != 1:
+            raise ValueError(
+                "Invalid polynomial string to parse: '=' should be present exactly once.")
+        if polynom_str.startswith("=") or polynom_str.endswith("="):
+            raise ValueError(
+                "Invalid polynomial string to parse: '=' should not be at the beginning or end.")
         polynom_str = polynom_str.strip()
+        polynom_str = polynom_str.replace("\n", "")
         polynom_str = polynom_str.replace(" ", "")
         polynom_str = polynom_str.replace("x", "X")
-        if re.match(self.pattern, polynom_str) is None:
-            raise ValueError("Invalid polynomial string to parse: contains invalid characters.")
+        if re.match(cls.pattern, polynom_str) is None:
+            raise ValueError(
+                "Invalid polynomial string to parse: contains invalid characters.")
         return polynom_str
 
+    @classmethod
+    def parse(cls, polynomial_str: str):
+        """Parse a polynomial string into a list of PolynomialTerm objects."""
+        polynomial_str = cls.normalize(polynomial_str)
+        # Now we need to transfer terms from after equal sign to before with sign change
+        polynomial_left, polynomial_right = polynomial_str.split("=")
 
-    def parse(self):
-        """Parse the polynomial string into list of terms."""
         # Split the polynomial string into parts based on operators
-        pattern = r"(?=[=+\-])"
-        parts = self.polynom_str.split(pattern)
+        pattern = r"(?=[+\-])"
+        left_terms = re.split(pattern, polynomial_left)
+        right_terms = re.split(pattern, polynomial_right)
 
         terms = []
-        for part in parts:
-            if part:
-                terms.append(PolynomTerm.from_string(part))
+        for term in left_terms:
+            if not term:
+                continue
+            if term == "+" or term == "-":
+                raise ValueError("Invalid polynomial string: invalid operator sequence.")
+            terms.append(PolynomialTerm.from_string(term))
 
-            
-        
+        for term in right_terms:
+            if not term:
+                continue
+            new_term = PolynomialTerm.from_string(term)
+            new_term.coefficient *= -1
+            terms.append(new_term)
 
-
-
-        
-    
+        return terms
--- test/test_polynominal.py
+++ test/test_polynominal.py
@@ -1,56 +1,128 @@
 import pytest
-from polynominal import PolynomSecondDegree, PolynomTerm, PolynomParser
+from polynominal import PolynomialTerm, PolynomParser, Polynomial
 
 
-# def test_discriminant_property():
-#     poly = PolynomSecondDegree(1, -3, 2)
-#     # discriminant = (-3)^2 - 4*1*2 = 9 - 8 = 1
-#     assert poly.discriminant == 1
-
-# def test_solutions_count_two():
-#     poly = PolynomSecondDegree(1, 0, -1)  # x^2 - 1 = 0, discriminant = 4 > 0
-#     assert poly.sulutions_count() == 2
-
-# def test_solutions_count_one():
-#     poly = PolynomSecondDegree(1, 2, 1)  # x^2 + 2x + 1 = 0, discriminant = 0
-#     assert poly.sulutions_count() == 1
-
-# def test_solutions_count_zero():
-#     poly = PolynomSecondDegree(1, 0, 1)  # x^2 + 1 = 0, discriminant = -4 < 0
-#     assert poly.sulutions_count() == 0
-
-# def test_polynomparser_normalize_valid():
-#     parser = PolynomParser("2 * X^2 + 3 * X + 1 = 0")
-#     assert isinstance(parser.polynom_str, str)
-
-# def test_polynomparser_normalize_invalid():
-#     with pytest.raises(ValueError):
-#         PolynomParser("2 * X^2 + 3 * X + 1 = a")
-
-# def test_polynomparser_normalize_not_string():
-#     with pytest.raises(ValueError):
-#         PolynomParser(12345)
-
-data_polynomterm_from_string = [
-    {"input": "0", "expected": PolynomTerm(0, 0)},
-    {"input": "X", "expected": PolynomTerm(1, 1)},
-    {"input": "-X", "expected": PolynomTerm(-1, 1)},
-    {"input": "4", "expected": PolynomTerm(4, 0)},
-    {"input": "-5", "expected": PolynomTerm(-5, 0)},
-    {"input": "2*X", "expected": PolynomTerm(2, 1)},
-    {"input": "-2*X", "expected": PolynomTerm(-2, 1)},
-    {"input": "3*X^2", "expected": PolynomTerm(3, 2)},
-    {"input": "-3*X^2", "expected": PolynomTerm(-3, 2)},
-    {"input": "X^3", "expected": PolynomTerm(1, 3)},
-    {"input": "-X^3", "expected": PolynomTerm(-1, 3)},
-    {"input": "5*X^4", "expected": PolynomTerm(5, 4)},
-    {"input": "-5*X^4", "expected": PolynomTerm(-5, 4)},
+data_polynom_term_positive_string = [
+    {"input": "-0", "expected": PolynomialTerm(0, 0)},
+    {"input": "+0", "expected": PolynomialTerm(0, 0)},
+    {"input": "0", "expected": PolynomialTerm(0, 0)},
+    {"input": "X", "expected": PolynomialTerm(1, 1)},
+    {"input": "+X", "expected": PolynomialTerm(1, 1)},
+    {"input": "-X", "expected": PolynomialTerm(-1, 1)},
+    {"input": "+4", "expected": PolynomialTerm(4, 0)},
+    {"input": "4", "expected": PolynomialTerm(4, 0)},
+    {"input": "-5", "expected": PolynomialTerm(-5, 0)},
+    {"input": "X^2", "expected": PolynomialTerm(1, 2)},
+    {"input": "-X^2", "expected": PolynomialTerm(-1, 2)},
+    {"input": "2*X", "expected": PolynomialTerm(2, 1)},
+    {"input": "+2*X", "expected": PolynomialTerm(2, 1)},
+    {"input": "-2*X", "expected": PolynomialTerm(-2, 1)},
+    {"input": "3*X^2", "expected": PolynomialTerm(3, 2)},
+    {"input": "+3*X^2", "expected": PolynomialTerm(3, 2)},
+    {"input": "-3*X^2", "expected": PolynomialTerm(-3, 2)},
+    {"input": "X^3", "expected": PolynomialTerm(1, 3)},
+    {"input": "+X^3", "expected": PolynomialTerm(1, 3)},
+    {"input": "-X^3", "expected": PolynomialTerm(-1, 3)},
+    {"input": "5*X^4", "expected": PolynomialTerm(5, 4)},
+    {"input": "+5*X^4", "expected": PolynomialTerm(5, 4)},
+    {"input": "-5*X^4", "expected": PolynomialTerm(-5, 4)},
 ]
 
-@pytest.mark.parametrize("term_data", data_polynomterm_from_string)
-def test_polynomterm_from_string(term_data):
+
+@pytest.mark.parametrize("term_data", data_polynom_term_positive_string)
+def test_polynomial_term_from_string_positive(term_data):
     term_str = term_data["input"]
     expected_term = term_data["expected"]
-    term = PolynomTerm.from_string(term_str)
+    term = PolynomialTerm.from_string(term_str)
     assert term.coefficient == expected_term.coefficient
     assert term.degree == expected_term.degree
+
+
+def test_polynomial_term_eq():
+    term1 = PolynomialTerm(1, 2)
+    term2 = PolynomialTerm(1, 2)
+    assert term1 == term2
+    term1 = PolynomialTerm(1, 2)
+    term2 = PolynomialTerm(1, 3)
+    assert term1 != term2
+
+
+def test_polynomial_term_repr():
+    term = PolynomialTerm(1, 2)
+    assert repr(term) == "PolynomialTerm(1, 2)"
+
+
+def test_polynomial_term_str():
+    term = PolynomialTerm(1, 2)
+    assert str(term) == "1*X^2"
+    term = PolynomialTerm(0, 2)
+    assert str(term) == "0*X^2"
+    term = PolynomialTerm(-1, 2)
+    assert str(term) == "-1*X^2"
+    term = PolynomialTerm(1, 0)
+    assert str(term) == "1*X^0"
+    term = PolynomialTerm(0, 0)
+    assert str(term) == "0*X^0"
+    term = PolynomialTerm(-1, 0)
+    assert str(term) == "-1*X^0"
+
+
+data_polynom_parser_positive_string = [
+    {"input": "0=0", "expected": [PolynomialTerm(0, 0), PolynomialTerm(0, 0)]},
+    {"input": "X=0", "expected": [PolynomialTerm(1, 1), PolynomialTerm(0, 0)]},
+    {"input": "X^2=0", "expected": [PolynomialTerm(1, 2), PolynomialTerm(0, 0)]},
+    {"input": "X^2+X=0", "expected": [PolynomialTerm(1, 2), PolynomialTerm(1, 1), PolynomialTerm(0, 0)]},
+    {"input": "X^2+X^3=0", "expected": [PolynomialTerm(1, 2), PolynomialTerm(1, 3), PolynomialTerm(0, 0)]},
+    {"input": "X^2+X^3+X=-1", "expected": [PolynomialTerm(1, 2), PolynomialTerm(1, 3), PolynomialTerm(1, 1), PolynomialTerm(1, 0)]},
+    {"input": "2*X^2+3*X^3+4*X^4-1=0", "expected": [PolynomialTerm(2, 2), PolynomialTerm(3, 3), PolynomialTerm(4, 4), PolynomialTerm(-1, 0), PolynomialTerm(0, 0)]},
+    # String with spaces. Normalization test
+    {"input": "  2 *X ^ 2+ 3* X ^3+ 4 * x ^ 4 -1 = 0  ", "expected": [PolynomialTerm(2, 2), PolynomialTerm(3, 3), PolynomialTerm(4, 4), PolynomialTerm(-1, 0), PolynomialTerm(0, 0)]},
+    ]
+
+
+@pytest.mark.parametrize("polynom_data", data_polynom_parser_positive_string)
+def test_polynomial_parser_positive(polynom_data):
+    polynom_str = polynom_data["input"]
+    expected_polynom = polynom_data["expected"]
+    polynom = PolynomParser.parse(polynom_str)
+    assert polynom == expected_polynom
+
+
+data_polynom_parser_negative_string = [
+    {"input": "abc", "expected": "Invalid polynomial string"},
+    {"input": "X=abc", "expected": "Invalid polynomial string"},
+    {"input": "X=0=0", "expected": "Invalid polynomial string"},
+    {"input": "2X2X=3X", "expected": "Invalid term"},
+    {"input": "=3*X", "expected": "Invalid polynomial string"},
+    {"input": "X=^2", "expected": "Invalid term"},
+    {"input": "X++2=0", "expected": "Invalid polynomial string"},
+    {"input": "X--2=0", "expected": "Invalid polynomial string"},
+    {"input": "X+=0", "expected": "Invalid polynomial string"},
+    {"input": "X-=0", "expected": "Invalid polynomial string"},
+    {"input": "2*X*3=0", "expected": "Invalid term"},
+    {"input": "X*X=0", "expected": "Invalid term"},
+    {"input": "=X=0", "expected": "Invalid polynomial string"},
+    {"input": "X==0", "expected": "Invalid polynomial string"},
+    {"input": "=X=2", "expected": "Invalid polynomial string"},
+    {"input": "X=", "expected": "Invalid polynomial string"},
+    {"input": "=X", "expected": "Invalid polynomial string"},
+    {"input": "X+*2=0", "expected": "Invalid term"},
+    {"input": "X*+2=0", "expected": "Invalid term"},
+    {"input": "*X*2=0", "expected": "Invalid term"}]
+
+
+@pytest.mark.parametrize("polynom_data", data_polynom_parser_negative_string)
+def test_polynomial_parser_negative(polynom_data):
+    polynom_str = polynom_data["input"]
+    expected_error_message = polynom_data["expected"]
+    with pytest.raises(Exception) as e:
+        PolynomParser.parse(polynom_str)
+    assert expected_error_message in str(e.value)
+
+
+def test_polynomial(monkeypatch):
+    terms = [PolynomialTerm(1, 2), PolynomialTerm(1, 3), PolynomialTerm(1, 4)]
+    monkeypatch.setattr(Polynomial, "__abstractmethods__", set())
+    polynom = Polynomial(terms)
+    assert polynom.terms == terms
+    assert polynom.degree == 4
