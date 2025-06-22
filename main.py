cfea590aa6480d7d6f81a3b5db0db01e32dc692a
Golovanov, Konstantin <kgolovanov@itkey.com>
2025-06-22 21:58:36 +0200

    Add main program to parse and solve polynomial equations:
    - Introduced `main.py` with CLI support using `argparse`.
    - Implemented polynomial parsing and solving flow leveraging `PolynomParser` and `PolynomialFactory`.
    - Provided error handling and detailed output for reduced form, degree, discriminant, solutions count, and solutions.

---
+++ main.py
@@ -0,0 +1,32 @@
+import argparse
+from polynominal import PolynomParser, PolynomialFactory
+
+
+def init_argparse():
+    parser = argparse.ArgumentParser(
+        prog="ft_computor_v1",
+        description="program that solves a polynomial second or lower degree equation.",
+    )
+
+    parser.add_argument("equation", type=str, help="An equation to solve")
+    return parser
+
+
+def main():
+    polynomial_factory = PolynomialFactory(PolynomParser())
+    arg_parser = init_argparse()
+    args = arg_parser.parse_args()
+    try:
+        polynomial = polynomial_factory.create(args.equation)
+        print("Reduced form: ", polynomial.get_reduced_form())
+        print("Polynomial degree: ", polynomial.degree)
+        if polynomial.degree == 2:
+            print("Discriminant: ", polynomial.discriminant)
+        print("Solutions count: ", polynomial.solutions_count)
+        print("Solutions: ", polynomial.get_solution_string())
+    except Exception as e:
+        print("Error: " + str(e))
+
+
+if __name__ == "__main__":
+    main()
